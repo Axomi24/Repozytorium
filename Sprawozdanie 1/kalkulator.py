a536c148b915e838b663f1ed3ccaa135d9123b3a
Axomi <83697500+Axomi24@users.noreply.github.com>
2021-05-05 11:52:23 +0200

    Update kalkulator.py

--- Sprawozdanie 1/kalkulator.py
+++ Sprawozdanie 1/kalkulator.py
@@ -1,4 +1,3 @@
-print("hello world")
 print("==========")
 print("Kalkulator")
 print("==========")
@@ -6,22 +5,30 @@ print("Dodawanie 1")
 print("Odejmowanie 2")
 print("Mnożenie 3")
 print("Dzielenie 4")
+print("Potęgowanie 5")
 print("==========")
 x=int(input("Podaj x "))
 y=int(input("Podaj y "))
 print("==========")
-d=int(input("Podaj typ działania (1-4) "))
-if d==1:
+d=input("Podaj typ działania (1-5) ")
+if d=='1':
     wynik=x+y
-elif d==2:
+elif d=='2':
     wynik=x-y
-elif d==3:
+elif d=='3':
     wynik=x*y
-elif d==4:
+elif d=='4':
     if y!=0:
         wynik=x/y
     else:
         print("Błąd, nie dziel przez 0")
         input()
         exit()
+elif d=='5':
+    wynik=pow(x,y)
+else:
+    print("Błąd, zły typ działania")
+    input()
+    exit()
+
 print("wynik: "+str(wynik))
