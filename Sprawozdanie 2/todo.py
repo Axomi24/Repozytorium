313f558e4eaad233c6221eed6458dc9403626596
Axomi <83697500+Axomi24@users.noreply.github.com>
2021-05-12 14:09:16 +0200

    Alter + exceptions

--- Sprawozdanie 2/todo.py
+++ Sprawozdanie 2/todo.py
@@ -22,15 +22,52 @@ def draw():
 while True:
     clear()
     draw()
-    act=input("ACTION: ")
-    if act=='1':
+    
+    while True:
+        try:
+            act=int(input("ACTION: "))
+            break
+        except ValueError:
+            print('Enter a number. Try again.')
+    if act==1: #CREATE
         task=input("Describe the task: ")
-        tasks.append(Record(task))
-    elif act=='2':
-        ind=int(input("Which one?: "))
-        rem=tasks[ind-1]
-        del rem
-        tasks.pop(ind-1)
+        if task!='':
+            tasks.append(Record(task))
+        else:
+            print("Wrong value, You can't enter an empty task!")
+            input("Press enter to continue")
+    elif act==2: #DELETE
+        while True:
+            try:
+                ind=int(input("Delete? Which one?: "))
+                break
+            except ValueError:
+                print('Enter a number. Try again.')
 
-
-
+        if 1 <= ind <= len(tasks):
+            rem=tasks[ind-1]
+            del rem
+            tasks.pop(ind-1)
+        else:
+            print("Wrong value, Value is out of range")
+            input("Press enter to continue")
+    elif act==3: #ALTER
+        while True:
+            try:
+                ind=int(input("Alter? Which one?: "))
+                break
+            except ValueError:
+                print('Enter a number. Try again.')
+        if 1 <= ind <= len(tasks):
+            newtask=input("Describe the task: ")
+            if task!='':
+                tasks[ind-1].task=newtask
+            else:
+                print("Wrong value, You can't enter an empty task!")
+                input("Press enter to continue")
+        else:
+            print("Wrong value, Value is out of range")
+            input("Press enter to continue")
+    else: #DEFAULT
+        print("Wrong value, enter a value 1-3")
+        input("Press enter to continue")
