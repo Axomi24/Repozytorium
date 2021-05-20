0ebdf963937ba9248a6e945a8d92b7ca9006da34
Axomi <83697500+Axomi24@users.noreply.github.com>
2021-05-20 23:56:54 +0200

    ból i cierpienie

--- Sprawozdanie 4/MyProject/api_basic/urls.py
+++ Sprawozdanie 4/MyProject/api_basic/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import car_list, car_detail, CarAPIView, CarDetails
+from .views import car_list, car_detail, CarAPIView, CarDetails, GenericAPIView
 
 urlpatterns = [
     #path('car/', car_list),
     path('car/', CarAPIView.as_view()),
     #path('detail/<int:pk>/', car_detail),
     path('detail/<int:id>/', CarDetails.as_view()),
+    path('generic/car/<int:id>/', GenericAPIView.as_view()),
 ]
--- Sprawozdanie 4/MyProject/api_basic/views.py
+++ Sprawozdanie 4/MyProject/api_basic/views.py
@@ -8,7 +8,34 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework import mixins
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
+
+class GenericAPIView(generics.GenericAPIView,mixins.ListModelMixin,mixins.CreateModelMixin, mixins.UpdateModelMixin,mixins.RetrieveModelMixin,mixins.DestroyModelMixin):
+    serializer_class=CarSerializer
+    queryset=Car.objects.all()
+    lookup_field = 'id'
+
+    authentication_classes = [SessionAuthentication,BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+
+    def post(self,request):
+        return self.create(request)
+
+    def put(self,request, id=None):
+        return self.update(request,id)
+
+    def delete(self,request, id):
+        return self.destroy(request,id)
 
 class CarAPIView(APIView):
     def get(self, request):
@@ -23,69 +50,3 @@ class CarAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class CarDetails(APIView):
-    def get_object(self, id):
-        try:
-            return Car.objects.get(id=id)
-            
-        except Car.DoesNotExist:
-            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
-
-    def get(self, request, id):
-        car=self.get_object(id)
-        serializer= CarSerializer(car)
-        return Response(serializer.data)
-
-    def put(self, request, id):
-        car=self.get_object(id)
-        serializer=CarSerializer(car, data=request.data)
-        
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, id):
-        car=self.get_object(id)
-        car.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-@api_view(['GET', 'POST'])
-def car_list(request):
-    if request.method=='GET':
-        cars=Car.objects.all()
-        serializer= CarSerializer(cars, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer=CarSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def car_detail(request, pk):
-    try:
-        car = Car.objects.get(pk=pk)
-    
-    except Car.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method=='GET':
-        serializer= CarSerializer(car)
-        return Response(serializer.data)
-
-
-    elif request.method=='PUT':
-        serializer=CarSerializer(car, data=request.data)
-        
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method=='DELETE':
-        car.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
